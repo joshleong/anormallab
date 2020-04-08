3417d980f106e5d5cec54c01107115734d6884d3
Josh Leong <josh.leong@gmail.com>
2020-04-07 20:00:00 -0700

    Links!

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Link from 'next/link';
+
 export default function Index() {
     return (
         <div>
+            <Link href="/about"><a title="About Page">About Page</a></Link>
             <p>Hello next.js</p>
         </div>
     );
